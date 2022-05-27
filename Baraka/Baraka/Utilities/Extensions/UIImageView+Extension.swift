b237bbf7b31c0b5cdee98f2786bd6f0edbcf4478
jawad <ahmad_abdulgawad.mahmoud@yahoo.com>
2022-05-27 17:24:21 +0200

    `SDWebImage` pod added as an async image downloader with cache support.

---
+++ Baraka/Baraka/Utilities/Extensions/UIImageView+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  UIImageView+Extension.swift
+//  Baraka
+//
+//  Created by Ahmed Abduljawad on 27/05/2022.
+//
+
+import UIKit
+import SDWebImage
+
+extension UIImageView {
+    
+    func setImageWithIndicator(url: String?, completion: ((UIImage?)->())? = nil) {
+        guard let _ = url, let theUrl = URL(string: url!) else {
+            image = UIImage(named: "")
+            return
+        }
+        SDImageCache.shared.config.shouldCacheImagesInMemory = true
+        sd_imageIndicator = SDWebImageActivityIndicator.gray
+        sd_setImage(with: theUrl, placeholderImage: nil, options: .refreshCached, completed: { (downLoadedImage, error, cacheType, url) in
+            completion?(downLoadedImage)
+        })
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Baraka
+
+Third-party libraries:
+
+1: `SDWebImage` is an async image downloader with cache support. 
+
+WHY did I use? 
+1. I can use with it an activity indicator to show the status of downloading the images which improves the app user experience.
+2. Easy to integrate in code for example I use it on an extension of type `UIImageView` and it can also be used on other controls like UIButton,..
+3. It can be used as an async image downloader and to cache images in memory or it can only be used as an async image downloader. 
+4. So popular and has so good rating/stars on Github which means it'll be updated to date with Apple new OS updates continuously.
+3. Accurate memeory caching for images.
+
