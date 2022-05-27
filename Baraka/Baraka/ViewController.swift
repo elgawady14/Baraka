62bddde5aedd8f5ee844a2a98fe76822310634ab
jawad <ahmad_abdulgawad.mahmoud@yahoo.com>
2022-05-27 13:15:11 +0200

    Project construction.

---
+++ Baraka/Baraka/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Baraka
+//
+//  Created by Ahmed Abduljawad on 27/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
